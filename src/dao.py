64b484c2c6e86c431a56d5f8d037191ee3de63b6
gutz <01iVeR@inbox.ru>
2025-01-08 23:11:21 +0500

    moved crud requests to BaseDAO

--- src/dao.py
+++ src/dao.py
@@ -1,5 +1,8 @@
-from sqlalchemy import select
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy import select, delete, update
 from .database import async_session
+from .exceptions import NotFoundException
+
 
 class BaseDAO:
     """
@@ -37,4 +40,39 @@ class BaseDAO:
             )
             result = await session.execute(query)
             return result.scalar_one_or_none()
+
+    @classmethod
+    async def create(cls, **data):
+        async with async_session() as session:
+            async with session.begin():
+                new_author = cls.model(**data)
+                session.add(new_author)
+                await session.flush()
+                return jsonable_encoder(new_author)
+
+    @classmethod
+    async def delete(cls, object_id: int):
+        async with async_session() as session:
+            async with session.begin():
+                stmt = (
+                    delete(cls.model).
+                    filter_by(id=object_id)
+                )
+                await session.execute(stmt)
+                return
+
+    @classmethod
+    async def update(cls, object_id: int, **values):
+        async with async_session() as session:
+            async with session.begin():
+                stmt = (
+                    update(cls.model).
+                    filter_by(id=object_id).
+                    values(**values).
+                    returning(cls.model.id)
+                )
+                changed_id = await session.execute(stmt)
+                if changed_id.scalar() is None:
+                    raise NotFoundException
+                return
 
