5d5305a5a37b74948b88d1ec34f9a4c50ee1e715
gutz <01iVeR@inbox.ru>
2025-01-09 17:04:43 +0500

    made username column unique

--- src/users/models.py
+++ src/users/models.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = 'user'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str]
+    username: Mapped[str] = mapped_column(unique=True)
     is_admin: Mapped[bool] = mapped_column(default=False)
     hashed_password: Mapped[str]
     books = relationship('UserBook', back_populates='users', secondary='user_book')
